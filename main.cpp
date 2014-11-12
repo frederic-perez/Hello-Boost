7990b1d1e7f6c5b5cec41e5f5c128a67211faa0e
Frederic Perez <frederic.perez@alma3d.com>
2014-11-12 17:51:54 +0100

    Extensions to actually use binary Boost components

--- main.cpp
+++ main.cpp
@@ -2,10 +2,39 @@
 
 #include <iostream>
 
+#define BOOST_LIB_DIAGNOSTIC
+
+#include <boost/filesystem.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/system/config.hpp>
+
+void
+TestFileSystem(const std::string& a_filename)
+{
+
+	const std::string filename = "main.cpp";
+	std::cout << filename << " has a file_size of "
+		<< boost::filesystem::file_size(a_filename) << std::endl;
+}
+
+void
+TestLexicalCast()
+{
+	using boost::lexical_cast;
+	using boost::bad_lexical_cast;
+
+	const short value = lexical_cast<short>("66");
+	std::cout << "lexical_cast<short>(\"66\") is " << value << std::endl;
+}
+
 int
-main()
+main(int /*argc*/, char* argv[])
 {
 	std::cout << "Hello, world!" << std::endl;
+	const std::string exeFile = argv[0];
+
+	TestFileSystem(exeFile);
+
 	return EXIT_SUCCESS;
 }
 
