43d83e9fa7caed628d6a7f77e20324bb78f0e73a
Frederic Perez <frederic.perez@alma3d.com>
2014-11-20 14:56:24 +0100

    TestFileSystem: Bug fixed + ENH

--- main.cpp
+++ main.cpp
@@ -11,10 +11,37 @@
 void
 TestFileSystem(const std::string& a_filename)
 {
+	using namespace boost::filesystem;
 
-	const std::string filename = "main.cpp";
-	std::cout << filename << " has a file_size of "
-		<< boost::filesystem::file_size(a_filename) << std::endl;
+	// "Testing" a_filename
+
+	std::cout << a_filename << " has a file_size of "
+		<< file_size(a_filename) << " bytes" << std::endl;
+
+	std::cout << a_filename;
+	const path myPath1(a_filename);
+	if (myPath1.has_extension())
+		std::cout << " has the extension " << myPath1.extension();
+	else
+		std::cout << " does not have any extension";
+	std::cout << std::endl;
+
+	// "Testing" the current directory
+
+	const std::string dot = ".";
+	const path currentDir(dot);
+	const bool isDirectory = is_directory(currentDir);
+	std::cout << "is_directory(" << currentDir << ") = " << std::boolalpha
+		<< isDirectory << std::endl;
+
+	if (isDirectory) {
+		std::cout << "Directory contents:" << std::endl;
+		std::copy(
+			directory_iterator(currentDir), directory_iterator(),
+			std::ostream_iterator<directory_entry>(std::cout, "\n"));
+			// directory_iterator::value_type is directory_entry, which is converted
+			// to a path by the path stream inserter
+	}
 }
 
 void
