71bf97ae9e3e94e4a8e9dce702e9e9fbfe960f71
Frederic Perez <frederic.perez@gmx.us>
2018-05-14 16:13:25 +0200

    master :art: Petty changes (tabs to spaces)

--- main.cpp
+++ main.cpp
@@ -10,12 +10,12 @@ namespace spy {
 std::string
 Boost_version()
 {
-	return
+  return
     std::to_string(BOOST_VERSION / 100000) // major version
     + "."
     + std::to_string(BOOST_VERSION / 100 % 1000) // minor version
     + "."
-		+ std::to_string(BOOST_VERSION % 100); // patch level
+    + std::to_string(BOOST_VERSION % 100); // patch level
 }
 
 } // namespace spy
@@ -23,40 +23,40 @@ Boost_version()
 void
 ExamplesOfLexicalCast(const std::string& a_string)
 {
-	std::clog << "ExamplesOfLexicalCast(-) started..." << std::endl;
+  std::clog << "ExamplesOfLexicalCast(-) started..." << std::endl;
 
-	using boost::lexical_cast;
-	using boost::bad_lexical_cast;
+  using boost::lexical_cast;
+  using boost::bad_lexical_cast;
 
-	const std::string pad = "  ";
+  const std::string pad = "  ";
 
-	const int valueIntS = stoi(a_string);
-	std::cout << pad << "stoi(\"" << a_string << "\") is "
-		<< valueIntS << std::endl;
-	const int valueIntL = lexical_cast<int>(a_string);
-	std::cout << pad << "lexical_cast<int>(\"" << a_string << "\") is "
-		<< valueIntL << std::endl;
+  const int valueIntS = stoi(a_string);
+  std::cout << pad << "stoi(\"" << a_string << "\") is "
+    << valueIntS << std::endl;
+  const int valueIntL = lexical_cast<int>(a_string);
+  std::cout << pad << "lexical_cast<int>(\"" << a_string << "\") is "
+    << valueIntL << std::endl;
 
-	const short valueShortS = static_cast<short>(stoi(a_string));
-	std::cout << pad << "stoi(\"" << a_string << "\") is "
-		<< valueShortS << std::endl;
-	try {
-		const short valueShortL = lexical_cast<short>(a_string);
-		std::cout << pad << "lexical_cast<short>(\"" << a_string << "\") is "
-			<< valueShortL << std::endl;
-	} catch (const boost::bad_lexical_cast& e) {
-		std::cerr << pad << "Exception caught: " << e.what() << '\n';
-	}
+  const short valueShortS = static_cast<short>(stoi(a_string));
+  std::cout << pad << "stoi(\"" << a_string << "\") is "
+    << valueShortS << std::endl;
+  try {
+    const short valueShortL = lexical_cast<short>(a_string);
+    std::cout << pad << "lexical_cast<short>(\"" << a_string << "\") is "
+      << valueShortL << std::endl;
+  } catch (const boost::bad_lexical_cast& e) {
+    std::cerr << pad << "Exception caught: " << e.what() << '\n';
+  }
 
-	std::clog << "ExamplesOfLexicalCast(-) finished." << std::endl;
+  std::clog << "ExamplesOfLexicalCast(-) finished." << std::endl;
 }
 
 namespace {
 
 void
 Output(const std::string& a_description, const std::string& a_value) {
-	const std::string pad = "  ";
-	std::cout << pad << a_description << ": \"" << a_value << "\"" << std::endl;
+  const std::string pad = "  ";
+  std::cout << pad << a_description << ": \"" << a_value << "\"" << std::endl;
 }
 
 } // namespace
@@ -64,38 +64,38 @@ Output(const std::string& a_description, const std::string& a_value) {
 void
 ExamplesOfAlgorithmsString()
 {
-	std::clog << "ExamplesOfAlgorithmsString() started..." << std::endl;
+  std::clog << "ExamplesOfAlgorithmsString() started..." << std::endl;
 
-	std::string name = "  Alan Turing  ";
-	Output("Name (original)", name);
+  std::string name = "  Alan Turing  ";
+  Output("Name (original)", name);
 
-	using namespace boost::algorithm;
-	to_lower(name);
-	Output("Name (after to_lower)", name);
-	to_upper(name);
-	Output("Name (after to_upper)", name);
-	trim_right(name);
-	Output("Name (after trim_right)", name);
-	trim(name);
-	Output("Name (after trim)", name);
-	name.erase(remove_if(name.begin(), name.end(), isspace), name.end());
-	Output("Name (after isspace removal)", name);
+  using namespace boost::algorithm;
+  to_lower(name);
+  Output("Name (after to_lower)", name);
+  to_upper(name);
+  Output("Name (after to_upper)", name);
+  trim_right(name);
+  Output("Name (after trim_right)", name);
+  trim(name);
+  Output("Name (after trim)", name);
+  name.erase(remove_if(name.begin(), name.end(), isspace), name.end());
+  Output("Name (after isspace removal)", name);
 
-	std::clog << "ExamplesOfAlgorithmsString() finished." << std::endl;
+  std::clog << "ExamplesOfAlgorithmsString() finished." << std::endl;
 }
 
 int
 main(int /*argc*/, char* /*argv*/[])
 {
-	std::cout << "Hello, Boost (version " + spy::Boost_version() + ")!\n" << std::endl;
+  std::cout << "Hello, Boost (version " + spy::Boost_version() + ")!\n" << std::endl;
 
-	const std::string inputValue = "1234567890";
-	ExamplesOfLexicalCast(inputValue);
-	std::cout << std::endl;
+  const std::string inputValue = "1234567890";
+  ExamplesOfLexicalCast(inputValue);
+  std::cout << std::endl;
 
-	ExamplesOfAlgorithmsString();
-	std::cout << std::endl;
+  ExamplesOfAlgorithmsString();
+  std::cout << std::endl;
 
-	std::cout << "Bye, Boost!" << std::endl;
-	return EXIT_SUCCESS;
+  std::cout << "Bye, Boost!" << std::endl;
+  return EXIT_SUCCESS;
 }
